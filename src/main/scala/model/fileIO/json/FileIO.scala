89bb6e617c6313c467bd85211f099e8fc8a5026e
ju851zel <ju851zel@htwg-konstanz.de>
2019-01-06 14:44:24 +0100

    json file io modified(not working yet)

--- src/main/scala/model/fileIO/json/FileIO.scala
+++ src/main/scala/model/fileIO/json/FileIO.scala
@@ -1,36 +1,29 @@
 package model.fileIO.json
 
 import model.DeskInterface
-import model.component.Desk
-import model.component.component.component.Board
-import model.component.component.{BoardInterface, PlayerInterface, TileInterface}
+import model.component.component.{PlayerInterface, TileInterface}
 import model.fileIO.FileIOInterface
-import play.api.libs.json.{JsValue, Json, Writes}
-import util.UtilMethods
-
-import scala.collection.SortedSet
-import scala.io.Source
+import play.api.libs.json.{JsNumber, Json, Writes}
 
 class FileIO extends FileIOInterface {
 
   override def load: DeskInterface = {
     var desk: DeskInterface = null
-    val source: String = Source.fromFile("desk.json").getLines.mkString
-    val json: JsValue = Json.parse(source)
-    val amountOfPlayer = (json \ "desk" \ "amountOfPlayer").get.toString.toInt
-    var players = Set[PlayerInterface]
-    var bagOfTiles = Set[TileInterface]
-    var ssets = Set[SortedSet[TileInterface]]
-    for (playerNodes <- json \ "desk" \ "players"){
-      for (player <- playerNodes){
-        val playerName = (json \\ "name")(player).as[String]
-        val playerNumber = (json \\ "number")(player).as[Int]
-        val playerState = (json \\ "state")(player).as[String]
-        var board = Board(SortedSet[TileInterface]())
-
-      }
-    }
-
+    //    val source: String = Source.fromFile("desk.json").getLines.mkString
+    //    val json: JsValue = Json.parse(source)
+    //    val amountOfPlayer = (json \ "desk" \ "amountOfPlayer").get.toString.toInt
+    //    var players = Set[PlayerInterface]()
+    //    var bagOfTiles = Set[TileInterface]()
+    //    var ssets = Set[SortedSet[TileInterface]]()
+    //    for (playerNodes <- json \ "desk" \ "players") {
+    //      for (player <- playerNodes) {
+    //        val playerName = (json \\ "name") (player).as[String]
+    //        val playerNumber = (json \\ "number") (player).as[Int]
+    //        val playerState = (json \\ "state") (player).as[String]
+    //        var board = Board(SortedSet[TileInterface]())
+    //
+    //      }
+    //    }
     desk
   }
 
@@ -43,30 +36,34 @@ class FileIO extends FileIOInterface {
 
   implicit val tileWrites = new Writes[TileInterface] {
     def writes(tile: TileInterface) = Json.obj(
-      "value" -> tile.getValue,
-      "color" -> tile.getColor,
       "ident" -> tile.identifier
     )
   }
 
   implicit val playerWrites = new Writes[PlayerInterface] {
     def writes(player: PlayerInterface) = Json.obj(
-      "value" -> player.getName,
-      "color" -> player.getNumber,
-      "ident" -> player.getState,
-      "board" -> player.getTiles
+      "name" -> player.getName,
+      "number" -> player.getNumber,
+      "state" -> player.getState,
+      "board" -> Json.toJson(
+        for {
+          tile <- player.getTiles
+        } yield {
+          Json.toJson(tile)
+        }
+      )
     )
   }
 
   def deskToJson(desk: DeskInterface) = {
     Json.obj(
       "desk" -> Json.obj(
-        "amountOfPlayers" -> Json.toJson(desk.amountOfPlayers),
+        "amountOfPlayers" -> JsNumber(desk.amountOfPlayers),
         "bag" -> Json.toJson(
           for {
             tile <- desk.bagOfTiles
           } yield {
-            Json.toJson(4)
+            Json.toJson(tile)
           }
         )
       )
