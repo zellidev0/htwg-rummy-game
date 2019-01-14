2ccb9e231bcc6d6fcc99d0342b8ed5117f7cdddf
ju851zel <ju851zel@htwg-konstanz.de>
2019-01-14 12:09:22 +0100

    activated json impl through DI

--- src/main/scala/de/htwg/se/rummy/RummyModule.scala
+++ src/main/scala/de/htwg/se/rummy/RummyModule.scala
@@ -13,7 +13,7 @@ class RummyModule extends AbstractModule with ScalaModule {
     //    bind[TileInterface].to[Tile]
     //    bind[BoardInterface].to[Board]
     //    bind[PlayerInterface].to[Player]
-    bind[FileIOInterface].to[model.fileIO.xml.FileIO]
+    bind[FileIOInterface].to[model.fileIO.json.FileIO]
     //    bind(new TypeLiteral[Set[PlayerInterface]] {}).to(classOf[Player])
 
 
