b96ac240a38f5e3d17d3f6b9376ad43a8df80c40
julian <julianMBP>
2020-05-28 12:48:51 +0200

    reformated Player Service and added template classes to test

--- rummy-player-service/src/main/scala/PlayerService.scala
+++ rummy-player-service/src/main/scala/PlayerService.scala
@@ -6,7 +6,6 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
 import model.DeskInterface
-import model.deskComp.deskBaseImpl.Desk
 import model.fileIO.FileIOJson
 import play.api.libs.json._
 
@@ -24,25 +23,28 @@ object PlayerService {
   private val playerController = new PlayerController
   private val fileIo = new FileIOJson()
 
-  private val playerRoute: Route = pathPrefix("players") {
-    path("switchToNext")(post(entity(as[String]) { input =>
-      println("PlayerService - switch to next: " + Json.prettyPrint(Json.parse(input)))
-      complete(fileIo.jsonToDesk(Json.parse(input)) match {
-        case Some(value) => handleCorrect(playerController.switchToNextPlayer(value))
-        case None => handleWrong()
-      })
-    })) ~
-      path("add")(post(entity(as[String]) { input =>
-        println("PlayerService - Add request: " + Json.prettyPrint(Json.parse(input)))
-        val name = checkCorrectName(input)
-        complete(fileIo.jsonToDesk(Json.parse(input)) match {
-          case Some(value) if name.isDefined =>
-            println("PlayerService - Value:", value)
-            handleCorrect(playerController.addPlayerAndInit(value, name.get.toString(), 12))
-          case None => handleWrong()
-        })
-      }))
-  }
+  private val playerRoute: Route =
+    pathPrefix("players") {
+      path("switchToNext")(post(entity(as[String]) { input =>
+        val deskOption = fileIo.jsonToDesk(Json.parse(input))
+        val response = deskOption match {
+          case Some(value) => handleCorrect(playerController.switchToNextPlayer(value))
+          case None => handleWrong("Desk has wrong format")
+        }
+        complete(response)
+      })) ~
+        path("add")(post(entity(as[String]) { input =>
+          val name = checkCorrectName(input)
+          val deskOption = fileIo.jsonToDesk(Json.parse(input))
+          val response = deskOption match {
+            case Some(value) if name.isDefined =>
+              handleCorrect(playerController.addPlayerAndInit(value, name.get.toString(), 12))
+            case None =>
+              handleWrong("Name or desk are not correct")
+          }
+          complete(response)
+        }))
+    }
 
   private val bindingFuture = Http().bindAndHandle(playerRoute, INTERFACE, PORT)
 
@@ -54,8 +56,8 @@ object PlayerService {
   private def handleCorrect(c: DeskInterface) =
     HttpResponse(OK, entity = fileIo.deskToJson(c).toString())
 
-  private def handleWrong() =
-    HttpResponse(InternalServerError)
+  private def handleWrong(string: String) =
+    HttpResponse(InternalServerError, entity = string)
 
   private def kill() =
     bindingFuture.flatMap(_.unbind()).onComplete(_ => system.terminate())
---
+++ rummy-player-service/src/test/scala/PlayerServiceTest.scala
@@ -0,0 +1,72 @@
+//import model.DeskInterface
+//import model.deskComp.deskBaseImpl.deskImpl.{Board, Color, Player, Tile}
+//import model.deskComp.deskBaseImpl.{Desk, TileInterface}
+//import org.scalatest.{Matchers, WordSpec}
+//
+//import scala.collection.immutable.SortedSet
+//
+//class PlayerSpec extends WordSpec with Matchers {
+//  val emptyBoard: Board = Board(SortedSet[TileInterface]())
+//
+//  "A PlayerService" when {
+//    val playerService = PlayerService()
+//    val john          = Player("John", Board(SortedSet[TileInterface]()))
+//    val mike          = Player("Mike", Board(SortedSet[TileInterface]()))
+//    val desk: DeskInterface = Desk(
+//      List(mike, john),
+//      Set(
+//        Tile(1, Color.RED, 0),
+//        Tile(2, Color.RED, 0),
+//        Tile(3, Color.RED, 0),
+//        Tile(4, Color.RED, 0),
+//        Tile(5, Color.RED, 0),
+//        Tile(6, Color.RED, 0),
+//        Tile(7, Color.RED, 0),
+//        Tile(8, Color.RED, 0),
+//        Tile(9, Color.RED, 0),
+//        Tile(10, Color.RED, 0),
+//        Tile(11, Color.RED, 0),
+//        Tile(12, Color.RED, 0),
+//        Tile(1, Color.BLUE, 0),
+//        Tile(1, Color.YELLOW, 0),
+//        Tile(2, Color.BLUE, 0),
+//        Tile(2, Color.YELLOW, 0),
+//        Tile(3, Color.BLUE, 0),
+//        Tile(3, Color.YELLOW, 0),
+//        Tile(4, Color.BLUE, 0),
+//        Tile(4, Color.YELLOW, 0),
+//        Tile(5, Color.BLUE, 0),
+//        Tile(5, Color.YELLOW, 0),
+//        Tile(6, Color.BLUE, 0),
+//        Tile(6, Color.YELLOW, 0),
+//        Tile(7, Color.BLUE, 0),
+//        Tile(7, Color.YELLOW, 0),
+//        Tile(8, Color.BLUE, 0),
+//        Tile(8, Color.YELLOW, 0),
+//        Tile(9, Color.BLUE, 0),
+//        Tile(9, Color.YELLOW, 0),
+//        Tile(10, Color.BLUE, 0),
+//        Tile(10, Color.YELLOW, 0),
+//        Tile(11, Color.BLUE, 0),
+//        Tile(11, Color.YELLOW, 0),
+//        Tile(12, Color.BLUE, 0),
+//        Tile(12, Color.YELLOW, 0),
+//      ),
+//      Set[SortedSet[TileInterface]]()
+//    )
+//    "creating a Player called Mike and a Player called John" should {
+//      "return current pLayer name Mike" in {
+//        playerService.currentPlayerName(desk) should be("Mike")
+//      }
+//      "switching to next player" in {
+//        playerService.currentPlayerName(playerService.switchToNextPlayer(desk)) should be("John")
+//      }
+//    }
+//    "adding a new player" should {
+//      val desk1: DeskInterface = playerService.addPlayerAndInit(desk, "Carl", 12)
+//      "12 tiles should be at the player" in {
+//        desk1.getPlayerByName("Carl").get.board.tiles.size should be(12)
+//      }
+//    }
+//  }
+//}
