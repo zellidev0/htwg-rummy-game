44fc4eaeb1d6d3c2e7a5f3c39a7a2af22a664224
julian <julianMBP>
2020-06-01 13:10:54 +0200

    added wui to Rummy.scala

--- rummy-main-service/src/main/scala/rummy/Rummy.scala
+++ rummy-main-service/src/main/scala/rummy/Rummy.scala
@@ -3,9 +3,8 @@ package rummy
 import model.DeskInterface
 import model.deskComp.deskBaseImpl.{Desk, PlayerInterface, TileInterface}
 import rummy.controller.impl
-import rummy.controller.impl.Controller
 import rummy.ui.UIConnector
-import rummy.ui.impl.{Gui, Tui}
+import rummy.ui.impl.{Tui, Wui}
 import rummy.util.{AnswerState, ControllerState}
 
 import scala.collection.immutable.SortedSet
@@ -13,23 +12,24 @@ import scala.collection.immutable.SortedSet
 object Rummy {
 
   val desk: DeskInterface = Desk(
-      players = List[PlayerInterface](),
-      bagOfTiles = Set[TileInterface](),
-      table = Set[SortedSet[TileInterface]]())
+    players = List[PlayerInterface](),
+    bagOfTiles = Set[TileInterface](),
+    table = Set[SortedSet[TileInterface]]())
 
   UIConnector.updateController(impl.Controller(desk, AnswerState.CREATE_DESK, ControllerState.MENU))
 
-  val gui  = new Gui(UIConnector)
-//  val akka = new Akka(UIConnector)
-  var tui  = new Tui(UIConnector)
+  //  val gui  = new Gui(UIConnector)
+  val akka = Wui()
+  var tui = new Tui(UIConnector)
+
   def main(args: Array[String]): Unit = {
     println("Type <c> to create a new desk or <l> to load a previous game")
     while (true) {
       val line = scala.io.StdIn.readLine();
       line match {
-        case ""  =>
+        case "" =>
         case "q" => return
-        case x   => tui.processInput(x)
+        case x => tui.processInput(x)
       }
     }
   }
