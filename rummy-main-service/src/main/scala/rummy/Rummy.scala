9a0fc3dd7f3ebe17c42f56ed0d2ea30f0ca3f062
julian <julianMBP>
2020-06-01 14:51:00 +0200

    commented out tui

--- rummy-main-service/src/main/scala/rummy/Rummy.scala
+++ rummy-main-service/src/main/scala/rummy/Rummy.scala
@@ -20,12 +20,13 @@ object Rummy {
 
   //  val gui  = new Gui(UIConnector)
   val akka = Wui()
-//  var tui = new Tui(UIConnector)
+  var tui = new Tui(UIConnector)
 
   def main(args: Array[String]): Unit = {
     println("Type <c> to create a new desk or <l> to load a previous game")
     while (true) {
-      val line = scala.io.StdIn.readLine();
+//      val line = scala.io.StdIn.readLine();
+      val line = "";
       line match {
         case "" =>
         case "q" => return
